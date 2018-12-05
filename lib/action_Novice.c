fd5e4c8600b53385cbd8e918a5339d0c1ae80f55
Gyumin Sim <rbalsdl815@naver.com>
2018-12-05 23:54:25 +0900

    Added Novice Workout Program Kinds

--- lib/action_Novice.c
+++ lib/action_Novice.c
@@ -3,18 +3,74 @@
 #include <string.h>
 #include "actnovice.h"
 #include "/usr/include/mysql/mysql.h"
+#include "actdb.h"
+
 #define DB_IP "222.121.186.100"
 
+void CreateJournal(MYSQL *connect)
+{
+	int choice;
+	int choice2;
+	char AddToExercise[10];
+	char buf[256];
+	char *user_id;
+	char *real_user_id;
+	MYSQL_RES *myresult;
+	MYSQL_ROW row;
 
-void goNovice(void)
+	//해당 유저의 아이디 정보를 가져온다.
+	mysql_query(connect,"select user()");
+	myresult = mysql_store_result(connect);
+	row = mysql_fetch_row(myresult);
+	user_id = (char *)row[0];
+	real_user_id = strtok(user_id,"@");
+
+
+	while(1)
+	{
+		printf(" 1. Create a Journal  2. Add Workout Exercise 3.Back \n");
+		printf("Enter : ");
+		scanf("%d", &choice);
+
+		if(choice == 1)
+		{
+		}
+		else if(choice == 2)
+		{
+			while(1)
+			{
+				printf(" What kind of exercise do you want to add? \n");
+				printf("Enter : ");
+				scanf("%s",AddToExercise);
+ 		
+		
+
+				sprintf(buf,"alter table workout.%s add %s char(15)",real_user_id,AddToExercise);
+				mysql_query(connect,buf);
+				memset(buf,0,sizeof(buf));
+
+				printf(" 1.Continue Add   2.No \n");
+				printf("Enter : ");
+				scanf("%d",&choice2);
+				if(choice2 == 2)
+				{
+					break;
+				}
+			}
+
+		}
+		else
+		{	
+			return;
+		}
+	}
+}
+
+
+void goNovice(MYSQL **connect)
 {
 	int choice = 0;
-	MYSQL *cons = mysql_init(NULL);
-	if(access_login(&cons)) {
-		printf("login error\n");
-		return 0;
-	}//login
-	
+
 	
 	printf("================Welcome Novice==================\n");
 	while(1)
@@ -24,7 +80,7 @@ void goNovice(void)
 		scanf("%d",&choice);
 		if(choice ==1)
 		{
-	
+			CreateJournal(*connect);
 		}
 		else if(choice ==2)
 		{
