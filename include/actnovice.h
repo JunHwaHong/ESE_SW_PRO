152aa75f119071517941db69c12a16798d272bfc
Gyumin Sim <rbalsdl815@naver.com>
2018-12-07 23:59:20 +0900

    almost complete Novice Part

--- include/actnovice.h
+++ include/actnovice.h
@@ -1,7 +1,7 @@
 #ifndef ACTNOVICE_H
 #define ACTNOVICE_H
 #include "/usr/include/mysql/mysql.h"
-
+void ViewJournal(MYSQL *connect);
 void goNovice(MYSQL **connect);
 void CreateJournal(MYSQL *connect);
 #endif
